52b3ddd0fb7fa3e50ce33637e675db2fc7c40177
Barry Griffith <barrygriffith@outlook.com>
2023-03-24 17:14:40 -0400

    Chapter 6 complete

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -19,7 +19,22 @@ export const fetchRequests = () => {
         )
 }
 
+export const sendRequest = (userServiceRequest) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userServiceRequest)
+    }
 
+
+    return fetch(`${API}/requests`, fetchOptions)
+        .then(response => response.json())
+        .then(() => {
+
+        })
+}
 
 export const getRequests = () => {
     return applicationState.requests.map(request => ({...request}))
